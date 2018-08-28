e3f6a1b21b9cdbc0a15b2ba987854a73a387627e
Darya Hlinskaya <Darya.Hlinskaya@itechart-group.com>
2018-08-28 17:08:30 +0300

    Start MERN

--- server/index.ts
+++ server/index.ts
@@ -3,12 +3,17 @@ var cors = require('cors');
 const app = express();
 const users = require('./routes/users');
 const feed = require('./routes/feed');
+import * as path from 'path';
 app.use(cors())
 app.set('port', (process.env.PORT || 5000));
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static("client/public"))
+    app.use(express.static("../client/build"))
 }
-app.use(express.static(__dirname + '/client/public'));
+app.use(express.static(__dirname + '../client/build'));
+app.get('/', function (req, res) {
+    console.log(__dirname)
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 
 app.use('/users', users);
 app.use('/', feed);
