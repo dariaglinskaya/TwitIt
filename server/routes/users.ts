9dbd911f646a95d0854e5f2e0222f8a41b03de80
Darya Hlinskaya <Darya.Hlinskaya@itechart-group.com>
2018-08-29 17:26:53 +0300

    test commit. trying to maintain heroku app

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -26,8 +26,8 @@ router.post('/login', passport.authenticate('login'), (req, res) => {
     console.log(req.user)
     if (req.user) {
         console.log('true')
-        //res.send(req.user);
-        return res.sendStatus(200).end();
+        res.send(req.user);
+        return res.sendStatus(200);
     } else {
         return res.sendStatus(401);
     }
