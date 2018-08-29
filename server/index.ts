123d14ffccb4d46e82247c8d450d3255406438c3
Darya Hlinskaya <Darya.Hlinskaya@itechart-group.com>
2018-08-29 12:23:26 +0300

    Add commands

--- server/index.ts
+++ server/index.ts
@@ -7,8 +7,10 @@ import * as path from 'path';
 app.use(cors())
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '../client/public'));
-
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 app.use('/users', users);
 app.use('/', feed);
 app.listen(app.get('port'), () => console.log('Node app is running on port', app.get('port')));
