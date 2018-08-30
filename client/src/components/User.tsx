d47b3ca95d9178ed696ba8a6a353f871272b4044
Darya Hlinskaya <Darya.Hlinskaya@itechart-group.com>
2018-08-30 14:34:09 +0300

    Add subscribe button to userpage

--- client/src/components/User.tsx
+++ client/src/components/User.tsx
@@ -1,24 +1,52 @@
-import { Col, Icon} from 'antd';
+import { Col, Icon, Button } from 'antd';
 import * as React from 'react';
 import Footer from './Footer';
 import Header from '../containers/Header';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import UserFeed from '../containers/UserFeed';
+import userActions from '../actions/userActions';
+import { bindActionCreators } from 'redux';
 
 export interface IProps {
     authentication: any;
     tweets: any;
     match: any;
     renderUserTweets: any;
+    subscribe: any;
+    unsubscribe: any;
 }
 
-export class User extends React.Component<IProps, {}>{
+export interface IState {
+    subscribed: boolean;
+}
+
+export class User extends React.Component<IProps, IState>{
     private customStyle = {
         display: 'inline',
     };
-    
-    public render() {        
+    componentDidMount() {
+        if (this.props.authentication.user.subscriptions !== undefined) {
+            this.props.authentication.user.subscriptions.forEach((item) => {
+                if (item === this.props.match.params.username.substring(1)) {
+                    this.setState(() => ({ subscribed: true }));
+                } else {
+                    this.setState(() => ({ subscribed: false }));
+                }
+            });
+        }
+    }
+    public subscribe(event) {
+        event.preventDefault();
+        this.props.subscribe(this.props.match.params.username.substring(1), this.props.authentication.user.username);
+        this.setState(() => ({ subscribed: true }));
+    }
+    public unsubscribe(event) {
+        event.preventDefault();
+        this.props.unsubscribe(this.props.match.params.username.substring(1), this.props.authentication.user.username);
+        this.setState(() => ({ subscribed: false }));
+    }
+    public render() {
         return (
             !this.props.authentication.loggedIn ? (<Redirect to="/" />) :
                 <div className='App-body' >
@@ -31,6 +59,9 @@ export class User extends React.Component<IProps, {}>{
                             <div className="user-page">
                                 <Icon type="user" className="user-page-icon" />
                                 <span className="author" style={this.customStyle}>@{this.props.match.params.username.substring(1)}</span>
+                                {this.state.subscribed ?
+                                    <Button type="primary" onClick={e => this.unsubscribe(e)} >Unsubscribe</Button> :
+                                    <Button type="default" onClick={e => this.subscribe(e)}>Subscribe</Button>}
                             </div>
                         </Col>
                     </div>
@@ -45,4 +76,11 @@ const mapStateToProps = state => {
         tweets: state.tweets
     };
 };
-export default connect(mapStateToProps)(User);
+const mapDispatchToProps = dispatch => {
+    const subscribe = (name, admin) => userActions.subscribe(name, admin);
+    const unsubscribe = (name, admin) => userActions.unsubscribe(name, admin);
+    return {
+        ...bindActionCreators({ subscribe, unsubscribe }, dispatch)
+    };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(User);
